11abaefd9bc430d422e051ffa9a16c87c40c5d82
psyGamer <manuel.preibisch@gmail.com>
2020-11-20 12:13:42 +0100

    Removed Margin + Padding from Presentation Window

--- Library/TypeScript/present.ts
+++ Library/TypeScript/present.ts
@@ -1,6 +1,30 @@
+// * Constants * //
+
+const presentationWindowID = "presentation-window";
+
+// * Classes * //
+
 class PresentationWindow {
-    constructor() {}
+    obj: HTMLElement | null;
+    body: HTMLElement | null;
+
+    x: number;
+    y: number;
+
+    constructor() {
+        this.obj = document.getElementById(presentationWindowID);
+
+        this.obj?.setAttribute(
+            "style",
+            "width: 100vw; height: 100vh; margin: 0; padding: 0; display: inline-block"
+        );
+
+        this.x = this.obj?.offsetWidth ? this.obj.offsetWidth : 0;
+        this.y = this.obj?.offsetHeight ? this.obj.offsetHeight : 0;
+    }
 }
+
+// * PresentJS Instance * //
 
 const PresentJS = {
     window: new PresentationWindow(),
--- Library/present.js
+++ Library/present.js
@@ -1,8 +1,17 @@
+// * Constants * //
+var presentationWindowID = "presentation-window";
+// * Classes * //
 var PresentationWindow = /** @class */ (function () {
     function PresentationWindow() {
+        var _a, _b, _c;
+        this.obj = document.getElementById(presentationWindowID);
+        (_a = this.obj) === null || _a === void 0 ? void 0 : _a.setAttribute("style", "width: 100vw; height: 100vh; margin: 0; padding: 0; display: inline-block");
+        this.x = ((_b = this.obj) === null || _b === void 0 ? void 0 : _b.offsetWidth) ? this.obj.offsetWidth : 0;
+        this.y = ((_c = this.obj) === null || _c === void 0 ? void 0 : _c.offsetHeight) ? this.obj.offsetHeight : 0;
     }
     return PresentationWindow;
 }());
+// * PresentJS Instance * //
 var PresentJS = {
     window: new PresentationWindow()
 };
