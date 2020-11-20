c840f8af0d32765dd87bb6bc84042cdf9f231b99
psyGamer <manuel.preibisch@gmail.com>
2020-11-20 22:00:23 +0100

    To be honest: I don't know what i specificly changed. Sorry

--- Library/TypeScript/present.ts
+++ Library/TypeScript/present.ts
@@ -1,18 +1,19 @@
 // * Constants * //
 
-const presentationWindowID = "presentation-window";
-
 // * Classes * //
 
 class PresentationWindow {
     obj: HTMLElement | null;
-    body: HTMLElement | null;
 
-    x: number;
-    y: number;
+    readonly x: number;
+    readonly y: number;
 
-    constructor() {
-        this.obj = document.getElementById(presentationWindowID);
+    id: string;
+
+    scenes: Scene[];
+
+    constructor(id: string) {
+        this.obj = document.querySelector(`presentation#${id}`);
 
         this.obj?.setAttribute(
             "style",
@@ -21,11 +22,94 @@ class PresentationWindow {
 
         this.x = this.obj?.offsetWidth ? this.obj.offsetWidth : 0;
         this.y = this.obj?.offsetHeight ? this.obj.offsetHeight : 0;
+
+        this.id = id;
+
+        this.scenes = Scene.getScenes(this);
+    }
+
+    static getWindows(): PresentationWindow[] {
+        const presentationElements: NodeList = document.querySelectorAll(
+            "presentation"
+        );
+
+        const presentations: PresentationWindow[] = [];
+
+        for (let i = 0; i < presentationElements.length; i++) {
+            presentations.push(
+                new PresentationWindow(
+                    (<HTMLElement>presentationElements[i]).id
+                )
+            );
+        }
+
+        return presentations;
+    }
+
+    static getWindowByID(windowID: string): PresentationWindow | null {
+        PresentJS.windows.forEach((window: PresentationWindow) => {
+            if (window.id == windowID) {
+                return window;
+            }
+        });
+
+        return null;
     }
 }
+
+// TODO
+
+class Scene {
+    elements: SceneElement[] = [];
+    parent: PresentationWindow | null;
+    title: string;
+
+    constructor(parent: PresentationWindow, sceneTitle: string) {
+        this.parent = parent;
+        this.title = sceneTitle;
+    }
+
+    static doesNameExist(windowID: string, sceneTitle: string): boolean {
+        const window: PresentationWindow | null = PresentationWindow.getWindowByID(
+            windowID
+        );
+
+        window?.scenes.forEach((scene: Scene) => {
+            if (scene.title == sceneTitle) {
+                return true;
+            }
+        });
+
+        return false;
+    }
+
+    static getScenes(window: PresentationWindow): Scene[] {
+        const sceneElements: NodeList = document.querySelectorAll(
+            `presentation#${window.id} scene`
+        );
+
+        const scenes: Scene[] = [];
+
+        for (let i = 0; i < sceneElements.length; i++) {
+            const sceneTitle: any = (<HTMLElement>(
+                sceneElements[i]
+            )).getAttribute("title");
+
+            if (sceneTitle) {
+                scenes.push(new Scene(window, sceneTitle));
+            }
+        }
+
+        return scenes;
+    }
+}
+
+class SceneElement {}
 
 // * PresentJS Instance * //
 
 const PresentJS = {
-    window: new PresentationWindow(),
+    windows: PresentationWindow.getWindows(),
 };
+
+// * Testing * //
--- Library/present.js
+++ Library/present.js
@@ -1,17 +1,69 @@
 // * Constants * //
-var presentationWindowID = "presentation-window";
 // * Classes * //
 var PresentationWindow = /** @class */ (function () {
-    function PresentationWindow() {
+    function PresentationWindow(id) {
         var _a, _b, _c;
-        this.obj = document.getElementById(presentationWindowID);
+        this.obj = document.querySelector("presentation#" + id);
         (_a = this.obj) === null || _a === void 0 ? void 0 : _a.setAttribute("style", "width: 100vw; height: 100vh; margin: 0; padding: 0; display: inline-block");
         this.x = ((_b = this.obj) === null || _b === void 0 ? void 0 : _b.offsetWidth) ? this.obj.offsetWidth : 0;
         this.y = ((_c = this.obj) === null || _c === void 0 ? void 0 : _c.offsetHeight) ? this.obj.offsetHeight : 0;
+        this.id = id;
+        this.scenes = Scene.getScenes(this);
     }
+    PresentationWindow.getWindows = function () {
+        var presentationElements = document.querySelectorAll("presentation");
+        var presentations = [];
+        for (var i = 0; i < presentationElements.length; i++) {
+            presentations.push(new PresentationWindow(presentationElements[i].id));
+        }
+        return presentations;
+    };
+    PresentationWindow.getWindowByID = function (windowID) {
+        PresentJS.windows.forEach(function (window) {
+            if (window.id == windowID) {
+                return window;
+            }
+        });
+        return null;
+    };
     return PresentationWindow;
+}());
+// TODO
+var Scene = /** @class */ (function () {
+    function Scene(parent, sceneTitle) {
+        this.elements = [];
+        this.parent = parent;
+        this.title = sceneTitle;
+    }
+    Scene.doesNameExist = function (windowID, sceneTitle) {
+        var window = PresentationWindow.getWindowByID(windowID);
+        window === null || window === void 0 ? void 0 : window.scenes.forEach(function (scene) {
+            if (scene.title == sceneTitle) {
+                return true;
+            }
+        });
+        return false;
+    };
+    Scene.getScenes = function (window) {
+        var sceneElements = document.querySelectorAll("presentation#" + window.id + " scene");
+        var scenes = [];
+        for (var i = 0; i < sceneElements.length; i++) {
+            var sceneTitle = (sceneElements[i]).getAttribute("title");
+            if (sceneTitle) {
+                scenes.push(new Scene(window, sceneTitle));
+            }
+        }
+        return scenes;
+    };
+    return Scene;
+}());
+var SceneElement = /** @class */ (function () {
+    function SceneElement() {
+    }
+    return SceneElement;
 }());
 // * PresentJS Instance * //
 var PresentJS = {
-    window: new PresentationWindow()
+    windows: PresentationWindow.getWindows()
 };
+// * Testing * //
